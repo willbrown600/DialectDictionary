04982a8e8b3c69f787bde5342f91623cc699ff44
willbrown600 <46435043+willbrown600@users.noreply.github.com>
2020-07-13 19:00:38 +1000

    Add Translation Activities

--- app/src/main/java/com/example/dialectdictionary/TranslateActivity.java
+++ app/src/main/java/com/example/dialectdictionary/TranslateActivity.java
@@ -5,10 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class TranslateActivity extends AppCompatActivity {
 
-    @Override
+
+public class TranslateActivity extends AppCompatActivity implements Adapter.View {
+
+        //boolean getEnglishDef = true;
+        String wordToBeConverted = "";
+        //Create word through textView7
+        //EditText word = findViewById(R.id.textView7);
+        private Adapter.Presenter presenter;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_translate);
+            //EditText word = findViewById(R.id.textView7);
+            //Instantiate presenter as new MainPresenter constructor passing
+            //in a view parameter referring to the view itself and
+            // the context variable referring to the which page we are talking about.
+            presenter = new TranslatePresenter(this, getApplicationContext());
+
+
+
+        }
+
+        public void onSubmitButtonClicked(View view) {
+            EditText word = findViewById(R.id.textView7);
+            wordToBeConverted = word.getText().toString();
+            //Use presenter variable to access getTranslationWords function through MainPresenter.
+            presenter.getTranslationWords(wordToBeConverted);
+
+            //new DownloadURLTaskDef(this).execute(wordToBeConverted, getEnglishDef);
+
+        }
+
+        @Override
+        public  void goToNextPage(Word s){
+            Intent i = new Intent(this, Translation.class);
+            //Add the string word to the value "wordToBeConverted"
+            i.putExtra("word", wordToBeConverted);
+            i.putExtra("new_word", s);
+            startActivity(i);
+        }
+}
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translate);
@@ -17,5 +59,5 @@ public class TranslateActivity extends AppCompatActivity {
     public void StartMainActivity(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
-}
+    }*/
+
